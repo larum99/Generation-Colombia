f5bcba6aace33b9ae4250642012220116209badb
Laura <laura.barona@uao.edu.co>
2026-06-24 16:48:44 -0500

    ejercicio hackerrank 1

--- Semana 7/prueba en java/src/Main.java
+++ Semana 7/prueba en java/src/Main.java
@@ -1,3 +1,5 @@
+
+
 public class Principal {
     public static void main(String[] args) {
         // FRAGMENTO 1 - Promoción y tipos pequeños
@@ -24,6 +26,70 @@ public class Principal {
         int denominador = 2;
         double division = (double) numerador / denominador;
         System.out.println("División: " + division);  // ¿Qué imprime? - entero 3.0, pero el resultado es 3.5
+
+        // switch tradicional case-break
+
+        // Switch moderno
+        int dia =  1;
+        String nombreDia = switch (dia) {
+            case 1, 7 -> "fin de semana";
+            case 2, 3, 4, 5, 6 -> "día laboral";
+            default -> "día desconocido";
+        };
+
+        System.out.println(nombreDia);
+
+        int codigoDescuento = 2;
+        double descuentoFinal = switch (codigoDescuento) {
+            case 1 -> 0.10;
+            case 2 -> {
+                System.out.println("Aplicando cupón");
+                double base = 0.15;
+                yield base + 0.05;
+            }
+            default -> {
+                System.out.println("Código");
+                yield 0.0;
+            }
+        };
+        System.out.println(descuentoFinal);
+
+        // switch - yield o asignación directa
+        int diaX = 3;
+        String cadenaDia = switch (diaX) {
+            case 1 -> "lunes";
+            case 2 -> {
+                System.out.println("Es martes de ofertas");
+                yield "martes";
+            }
+            default -> "otro día";
+        };
+        System.out.println(cadenaDia);
+
+        // while y do - while
+
+        int y = 5;
+
+        while (y < 5) {
+            System.out.println("En while");
+        }
+
+        do {
+            System.out.println("En do while");
+        } while (y < 5);
+
+
+        // entrada de datos
+        Scanner sc = new Scanner(System.in);
+        String entradaDatos;
+
+        do {
+            System.out.println("Presiona sí para continuar");
+            entradaDatos = sc.nextLine();
+        } while (!entradaDatos.equals("sí"));
+
+        System.out.println("Gracias");
+        sc.close();
     }
 }
 
